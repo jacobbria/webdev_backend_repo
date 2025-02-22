9ac7cb7ca872b6ba6a3360a32ea2b75388686f16
CJLA <39108639+CJLA@users.noreply.github.com>
2025-02-22 00:13:26 -0600

    change-package-name

--- src/main/java/com/franklin/techblog/FranklinTechBlogApplication.java
+++ src/main/java/com/franklin/techblog/FranklinTechBlogApplication.java
@@ -1,11 +1,10 @@
-package com.example;
+package com.franklin.techblog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
-public class Application {
+public class FranklinTechBlogApplication {
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        SpringApplication.run(FranklinTechBlogApplication.class, args);
     }
 }
